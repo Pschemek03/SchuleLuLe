88c55f64892478dd767940afad8a7e0374610ee6
Pschemek03 <przemyslaw.gawlik03@gmail.com>
2024-09-18 12:47:33 +0200

    gnna be workin with dat

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import Aufgaben.iKaufEinkauf;
+import Aufgaben.blutSpendeKlinik;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("welcher Programm soll jtz ran? \ncase 1 iKauf \ncase 2 blutSpende" );
+        Scanner sc = new Scanner(System.in);
+        var wert = sc.nextInt();
+
+        switch (wert){
+            case 1:
+                iKaufEinkauf.einkauf();
+                break;
+            default: System.out.println("Die Aufgabe gibt es ja nicht man, tschill");
+        }
+
+    }
+}
