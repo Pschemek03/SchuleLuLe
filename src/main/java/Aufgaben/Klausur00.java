36f6425acc2a74ced06e9374ba0443d7f4b6facd
pschemekg@gmail.com <pschemekg@gmail.com>
2024-12-17 10:04:45 +0100

    Merge remote-tracking branch 'origin/master' pokedex 1.0 ✅
    Signed-off-by: pschemekg@gmail.com <pschemekg@gmail.com>

---
+++ src/main/java/Aufgaben/Klausur00.java
@@ -0,0 +1,21 @@
+package Aufgaben;
+
+import java.util.Scanner;
+
+public class Klausur00 {
+    public static void main(String[] args) {
+        var sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        var number = sc.nextInt();
+
+        var summ = 1;
+
+        for (int i = 2; i <= number; i++) {
+            summ *= i;
+
+            System.out.println(summ + "\n");
+        }System.out.println("\nThe sum is: " + summ);
+
+    }
+}
--- src/main/java/Aufgaben/Main.java
+++ src/main/java/Aufgaben/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("welcher Programm soll jtz ran? \n1. iKauf \n2. blutSpende \n3. PokedexGUI \nZwischenSummevonZahlen, \n3DartWurf");
+        System.out.println("welcher Programm soll jtz ran? \n1. iKauf \n2. blutSpende \n3. PokedexGUI \nZwischenSummevonZahlen, \n3DartWurf" +
+                "\n4. KlausurAufgabe");
+
         var sc = new Scanner(System.in);
         var wert = sc.nextInt();
 
@@ -14,6 +16,7 @@ public class Main {
             case 3 -> PokedexGUI.main(args);
             case 4 -> KlausurVorbereitung00.main(args);
             case 5 -> KlausurVorbereitung01.main(args);
+            case 6 -> Klausur00.main(args);
             default -> System.out.println("Die Aufgabe gibt es ja nicht mal, tschill");
         }
     }
