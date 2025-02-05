458b05afe5455c6a0ef91e88184940aac39d05e9
pschemekg@gmail.com <pschemekg@gmail.com>
2025-02-05 12:22:17 +0100

    @School tday
    Signed-off-by: pschemekg@gmail.com <pschemekg@gmail.com>

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,12 @@
-import Aufgaben.Klausuren.KlausurVorbereitung01;
-import Aufgaben.Klausuren.Klausur00;
-import Aufgaben.Klausuren.KlausurVorbereitung00;
-import Aufgaben.PokedexGUI;
-import Aufgaben.bloodDonorsClinic;
-import Aufgaben.iKaufEinkauf;
+import Aufgaben.*;
+import Aufgaben.Klausuren.*;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcher Programm soll jtz ran? \n1. iKauf \n2. blutSpende \n3. PokedexGUI \n4. ZwischenSummevonZahlen, \n5. 3DartWurf" +
-                "\n6. KlausurAufgabe");
+                "\n6. KlausurAufgabe \n7. MethodenKopfAufgaben");
 
         var sc = new Scanner(System.in);
         var wert = sc.nextInt();
@@ -22,6 +18,7 @@ public class Main {
             case 4 -> KlausurVorbereitung00.main(args);
             case 5 -> KlausurVorbereitung01.main(args);
             case 6 -> Klausur00.main(args);
+            case 7 -> Aufgaben.MethodHeads.main(args);
             default -> System.out.println("Die Aufgabe gibt es ja nicht mal, tschill");
         }
     }
